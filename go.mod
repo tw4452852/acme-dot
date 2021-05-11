587aabc2d34a19a4ea509a58056f16cfe7a08c8a
Tw <wei.tan@intel.com>
2021-05-11 10:34:02 +0800

    It works
    Signed-off-by: Tw <wei.tan@intel.com>

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/tw4452852/acme-dot
+
+go 1.15
+
+require 9fans.net/go v0.0.4
---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"9fans.net/go/acme"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		log.Fatalf("%s winid\n", os.Args[0])
+	}
+
+	winid, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	w, err := acme.Open(winid, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer w.CloseFiles()
+
+	// get filename
+	tag, err := w.ReadAll("tag")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	i := bytes.IndexRune(tag, ' ')
+	if i < 0 {
+		i = len(tag)
+	}
+	filename := string(tag[:i])
+
+	// get current dot position
+	_, _, err = w.ReadAddr()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	err = w.Ctl("addr=dot")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	q0, q1, err := w.ReadAddr()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Printf("%s:#%d,#%d\n", filename, q0, q1)
+}
